70d309571b20ee5b586d3d5328137782224ff22a
IMalyanova <53153912+IMalyanova@users.noreply.github.com>
2019-08-05 01:13:13 +0300

    Update Loader.java

--- Hibernate/src/Loader.java
+++ Hibernate/src/Loader.java
@@ -9,33 +9,20 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Created by Danya on 26.10.2015.
- */
 public class Loader {
+    
     private static SessionFactory sessionFactory;
     public static String gap = "";
     private static Integer previous = 0;
 
     public static void main(String[] args) throws IOException {
+        
         setUp();
 
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
         List<HierarchicalTable> hierarTables = (List<HierarchicalTable>) session.createQuery("FROM HierarchicalTable").list();
-//        System.out.println("1. Вывести иерархический список:");
-//        for (HierarchicalTable element : hierarTables) {
-//            System.out.println(element.getParentId() + "  " + element.getName());
-//        }
-
-//        HierarchicalTable hierarchicalTable = new HierarchicalTable("Список");
-//        session.save(hierarchicalTable);
-//        System.out.println("Сохранен id = " + hierarchicalTable.getId());
-
-//        Department dept = (Department) session.createQuery("FROM Department WHERE name=:name")
-//            .setParameter("name", "Отдел производства").list().get(0);
-//        session.delete(dept);
 
         for (HierarchicalTable element : hierarTables) {
             listDirectory(element.getParentId(), element.getName());
@@ -44,7 +31,6 @@ public class Loader {
         session.getTransaction().commit();
         session.close();
 
-        //==================================================================
         if (sessionFactory != null) {
             sessionFactory.close();
         }
@@ -54,14 +40,13 @@ public class Loader {
     //=====================================================================
 
     private static void setUp() {
+        
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure(new File("src/config/hibernate.cfg.xml")) // configures settings from hibernate.config.xml
+                .configure(new File("src/config/hibernate.cfg.xml")) 
                 .build();
         try {
             sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         } catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
             StandardServiceRegistryBuilder.destroy(registry);
             throw e;
         }
